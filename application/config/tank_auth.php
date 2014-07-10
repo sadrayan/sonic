9f4aec4a9d0f4fecbf4505dc2ee6f96a85b8280d
sadrayan <sadrayan@gmail.com>
2014-07-09 22:27:27 -0400

    changing tank_auth

--- application/config/tank_auth.php
+++ application/config/tank_auth.php
@@ -7,7 +7,7 @@
 | These details are used in emails sent by authentication library.
 |--------------------------------------------------------------------------
 */
-$config['website_name'] = 'Sonic';
+$config['website_name'] = 'CoralTec';
 $config['webmaster_email'] = 'sadrayan@gmail.com';
 
 /*
@@ -41,12 +41,12 @@ $config['phpass_hash_strength'] = 8;
 */
 $config['allow_registration'] = TRUE;
 $config['captcha_registration'] = TRUE;
-$config['email_activation'] = TRUE;
+$config['email_activation'] = FALSE;
 $config['email_activation_expire'] = 60*60*24*2;
 $config['email_account_details'] = TRUE;
-$config['use_username'] = TRUE;
+$config['use_username'] = FALSE;
 
-$config['username_min_length'] = 4;
+$config['username_min_length'] = 5;
 $config['username_max_length'] = 20;
 $config['password_min_length'] = 8;
 $config['password_max_length'] = 20;
@@ -68,7 +68,7 @@ $config['password_max_length'] = 20;
 | 'login_attempt_expire' = Time to live for every attempt to login. Default is 24 hours (60*60*24).
 |--------------------------------------------------------------------------
 */
-$config['login_by_username'] = TRUE;
+$config['login_by_username'] = FALSE;
 $config['login_by_email'] = TRUE;
 $config['login_record_ip'] = TRUE;
 $config['login_record_time'] = TRUE;
